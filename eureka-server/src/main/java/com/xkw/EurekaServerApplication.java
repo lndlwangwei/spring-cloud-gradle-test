f31e8fd4975b6f3cf73923a7335ee1169f3238e3
wangwei <lndl.wangwei@gmail.com>
2019-07-02 17:16:07 +0800

    add eureka server

--- eureka-server/src/main/java/com/xkw/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/xkw/EurekaServerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Springboot1Application {
+public class EurekaServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Springboot1Application.class, args);
+        SpringApplication.run(EurekaServerApplication.class, args);
     }
 
 }
